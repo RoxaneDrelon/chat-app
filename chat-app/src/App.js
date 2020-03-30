9173890f47e388b7ebe5f29a5c2a42e7c5f6e800
RoxaneDrelon <temporarerare@gmail.com>
2020-03-30 18:09:23 +0200

    solution quest React 2.1

---
+++ chat-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Contact from './components/Contact';
+
+function App() {
+  return (
+    <div className="App">
+      <React.StrictMode>
+        <Contact />
+      </React.StrictMode>
+    </div>
+  );
+}
+
+export default App;
---
+++ chat-app/src/components/Contact.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./Contact.css";
+
+function Contact () {
+    return (
+        <div className="Contact">
+            <img className="avatar" src="https://randomuser.me/api/portraits/men/90.jpg" alt="Avatar" />
+            <div>
+                <h3 className="name">Hugh Simpson</h3>
+                <div className="status">
+                    <div className="status-online" />
+                    <p className="status-text">Online</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Contact;
